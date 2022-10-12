a2edd64db64ebc904ab509a73503efcd915a2bad
Leandro Ferreira <leandro.fcode@gmail.com>
2022-10-12 13:02:44 -0300

    mend
    fix fetch

--- src/scripts/counter.js
+++ src/scripts/counter.js
@@ -1,3 +1,5 @@
+// Html based declarations
+
 const init = document.getElementById('start')
 const finish = document.getElementById('end')
 const clockValue = document.getElementById('clock')
@@ -8,10 +10,15 @@ const container = document.querySelector('.container')
 
 const timeValueContainer = document.getElementById('timeContainer')
 
+// Local script based declarations
+
 let counter = 0
+let counterpd = 0
 var clockTime
 var boolean = false
-// TODO: Add audio file for notif/tune
+const pdcounters = []
+const DoneTune = new Audio('../assets/tunes/Apple pay .mp3')
+const back2Work = new Audio('../assets/tunes/Htc reactive.mp3')
 
 // Get values for clock
 init.addEventListener('click', () => {
@@ -19,6 +26,8 @@ init.addEventListener('click', () => {
     const isChecked = document.getElementById('check').checked
     const arr = []
     clearArray(arr)
+
+    back2Work.play()
     
     if(isChecked){
         const timePDInput = document.querySelectorAll('#timepd')
@@ -27,20 +36,35 @@ init.addEventListener('click', () => {
     } else pushArray(timeInput, arr)
     
     arr.forEach(element => {
+        const isPomodoro = element.getAttribute('id') == 'timepd'
+        const isTime = element.placeholder
+
+
         if(element.value != '') {
-            if(element.placeholder == 'hour') counter += element.value * 3600
-            else if(element.placeholder == 'minute') {
-                counter += element.value * 60
+            if(isTime == 'hour') {
+                if(isPomodoro) counterpd += element.value * 3600
+                else counter += element.value * 3600
             }
-            else counter += element.value * 1
+            else if(isTime == 'minute') {
+                if(isPomodoro) counterpd += element.value * 60
+                else counter += element.value * 60
+            }
+            else {
+                if(isPomodoro) counterpd += element.value * 1
+                else counter += element.value * 1
+            }
             element.value = ''
         }
     });
-    if(counter != 0) clock(counter)
+    if(counter != 0) {
+        if(isChecked && counterpd != 0) pdcounters.push(counter, counterpd)
+        clock(counter)
+    }
     counter = 0
+    counterpd = 0
 })
 
-// Update mode
+// Update modes before init
 checkValue.addEventListener('click', () => {
     const timePDInput = document.querySelectorAll('.pomodoro')
 
@@ -55,24 +79,31 @@ checkValue.addEventListener('click', () => {
 finish.addEventListener('click', () => {
     const isChecked = document.getElementById('check').checked
 
-    console.log(isChecked)
+    DoneTune.play()
 
     toggleClasses(init, 'inactive')
     toggleClasses(finish, 'inactive')
     toggleClasses(timeValueContainer, 'inactive')
     if(isChecked) toggleClasses(container, 'large')
+    clearArray(pdcounters)
     clearInterval(clockTime)
     clockValue.textContent = '00:00:00'
 })
 
 // Clock visual
-function clock(num, pdnum) {
+function clock(num) {
     const isChecked = document.getElementById('check').checked
+
+    const initIsVisible = init.classList.contains('inactive')
+    const finishIsVisible = finish.classList.contains('inactive')
+    const TimeContainerIsVisible = timeValueContainer.classList.contains('inactive')
     
-    toggleClasses(init, 'inactive')
-    toggleClasses(finish, 'inactive')
-    toggleClasses(timeValueContainer, 'inactive')
-    if(isChecked) toggleClasses(container, 'large')
+    if(!initIsVisible && finishIsVisible && !TimeContainerIsVisible){
+        toggleClasses(init, 'inactive')
+        toggleClasses(finish, 'inactive')
+        toggleClasses(timeValueContainer, 'inactive')
+        if(isChecked) toggleClasses(container, 'large')
+    }
 
     // Starts the clock
     clockTime = setInterval(() => {
@@ -90,20 +121,19 @@ function clock(num, pdnum) {
         num--
 
         if(num < 0) {
-            clearInterval(clockTime)
-            toggleClasses(init, 'inactive')
-            toggleClasses(finish, 'inactive')
-            toggleClasses(timeValueContainer, 'inactive')
-
-            // TODO: simplify init's components into functions to be used here
-            if(isChecked) {
-                const mode = toggleModes(boolean)
-            }
+            if(!isChecked) {
+                toggleClasses(init, 'inactive')
+                toggleClasses(finish, 'inactive')
+                toggleClasses(timeValueContainer, 'inactive')
+                clockValue.textContent = '00:00:00'
+                DoneTune.play()
+                clearInterval(clockTime)
+            } else toggleModes()
         }
-    }, 1000)
+    }, 100)
 }
 
-// Push items onto Array
+// Push array items onto new Array
 function pushArray(element, outputArray) {
     element.forEach(element => {
         outputArray.push(element)
@@ -116,9 +146,25 @@ function clearArray(array) {
     });
 }
 
-function toggleModes(boolean) {
-    if(boolean) return boolean = false
-    return boolean = true
+// Changes mode when in pomodoro
+function toggleModes() {
+    clearInterval(clockTime)
+    clockValue.textContent = '00:00:00'
+    
+    if(boolean) boolean = false
+    else boolean = true
+    
+    if(boolean) {
+        DoneTune.play()
+        clock(pdcounters[1])
+    }
+    else {
+        back2Work.play()
+        clock(pdcounters[0])
+    }
+
+    // False = counter
+    // True = rest
 }
 
 function toggleClasses(element, classItem) {
